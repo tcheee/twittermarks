d45daf18c9bee290319c2e1b74a61b4d94fbdfc6
Thomas Cherret <thomas.cherret@papernest.com>
2022-01-15 16:39:45 +0100

    second push

--- server/dist/authentication/twitter.strategy.d.ts
+++
@@ -1,7 +0,0 @@
-import { VerifyCallback } from 'passport-twitter';
-declare const TwitterStrategy_base: new (...args: any[]) => any;
-export declare class TwitterStrategy extends TwitterStrategy_base {
-    constructor();
-    validate(accessToken: string, secretToken: string, profile: any, done: VerifyCallback): Promise<any>;
-}
-export {};
