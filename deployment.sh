8539627a0899667ca409e7cd1f96883da02be7d0
Thomas Cherret <thomas.cherret@papernest.com>
2022-01-15 17:22:23 +0100

    test

--- deployment.sh
+++ deployment.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npm run build --prefix client && rm -rf server/build && sleep 1 && mv ./client/build ./server/ && git add . && git commit -m 'automatic deploy' & git push
+npm run build --prefix client && rm -rf server/build && sleep 1 && mv ./client/build ./server/
